3e758e9b6fc8f4f72429d45261dcb4557c3cc66c
pegueo2000 <pegueo2000@gmail.com>
2023-08-20 13:48:33 +0000

    quatrieme commit de pegue

--- src/test/java/org/edu/epo/MainTest.java
+++ src/test/java/org/edu/epo/MainTest.java
@@ -4,10 +4,11 @@ package org.edu.epo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class Main {
+public class MainTest {
     private final Calculator calculator = new Calculator();
     @Test
     void addition (){
         assertEquals(2, calculator.add(1,1));
+        assertEquals(12,calculator.add(3,9));
     }
 }
